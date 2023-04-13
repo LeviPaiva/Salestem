25a17b671b37bb51755e61beab3ca3c1e0ce13a4
Levi Paiva <levicpg@gmail.com>
2023-04-12 23:03:06 -0300

    Tentando colocar json

--- tableScript.js
+++ tableScript.js
@@ -74,6 +74,7 @@
 
 // let table = document.createElement("table");
 
+// let columnID = document.createElement("td");
 // let coluna1 = document.createElement("td");
 // let coluna2 = document.createElement("td");
 // let btnEdit = document.createElement("button");
@@ -83,18 +84,24 @@
 
 
 
-// coluna1.textContent = "Conteúdo da coluna 1";
-// coluna2.textContent = "Conteúdo da coluna 2";
+// coluna1.textContent = "Primeira Coluna";
+// coluna2.textContent = "Segunda Coluna";
 // btnEdit.textContent = "Editar";
 // btnDelete.textContent = "Deletar";
 
 // for (let i = 0; i < 5; i++) {
+  
+//   columnID.textContent = i;
+  
 //   let linha = document.createElement("tr");
+//   let novacolunaID = columnID.cloneNode(true);
 //   let novaColuna1 = coluna1.cloneNode(true);
 //   let novaColuna2 = coluna2.cloneNode(true);
 //   let novaColuna3 = btnEdit.cloneNode(true);
 //   let novaColuna4 = btnDelete.cloneNode(true);
   
+  
+
 //   novaColuna3.addEventListener("click", () => {
 //     if (novaColuna3.textContent === "Editar") {
 //       novaColuna1.innerHTML = '<input type="text" value="' + novaColuna1.textContent + '">';
@@ -104,6 +111,7 @@
 //     } else {
       
 //       let dadosLinha = {
+//         colunaId: novacolunaID.textContent,
 //         coluna1: novaColuna1.querySelector("input").value,
 //         coluna2: novaColuna2.querySelector("input").value
 //       };
@@ -120,8 +128,17 @@
 
 //   novaColuna4.addEventListener("click", () => {
 //     if (novaColuna4.textContent === "Cancelar") {
+
+//       if ( novaColuna1.querySelector("input").value !== novaColuna1.textContent ){
 //       novaColuna1.innerHTML = novaColuna1.querySelector("input").value;
 //       novaColuna2.innerHTML = novaColuna2.querySelector("input").value;
+//       }
+//      else{
+//       novaColuna1.innerHTML = coluna1.textContent;
+//       novaColuna2.innerHTML = coluna2.textContent;
+
+//      }
+      
 //       novaColuna3.textContent = "Editar";
 //       novaColuna4.textContent = "Deletar";
 //     } else {
@@ -129,6 +146,8 @@
 //     }
 //   });
   
+
+//   // linha.appendChild(novacolunaID);
 //   linha.appendChild(novaColuna1);
 //   linha.appendChild(novaColuna2);
 //   linha.appendChild(novaColuna3);
@@ -142,9 +161,111 @@
 // console.log(lt_data); 
 
 
+// =========================================================================================
+// =========================================================================================
+// =========================================================================================
 
 
+const fs = require('fs'); // Importa o módulo 'fs' para lidar com arquivos
+const data = [
+  {
+    "id": 1,
+    "coluna1": "Valor 1",
+    "coluna2": "Valor 2"
+  },
+  {
+    "id": 2,
+    "coluna1": "Valor 3",
+    "coluna2": "Valor 4"
+  },
+  {
+    "id": 3,
+    "coluna1": "Valor 5",
+    "coluna2": "Valor 6"
+  }
+];
 
+let table = document.createElement("table");
+
+let columnID = document.createElement("td");
+let coluna1 = document.createElement("td");
+let coluna2 = document.createElement("td");
+let btnEdit = document.createElement("button");
+let btnDelete = document.createElement("button");
+
+let lt_data = [];
+
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach((item, i) => {
+      // columnID.textContent = i;
+
+      let linha = document.createElement("tr");
+      let novacolunaID = columnID.cloneNode(true);
+      let novaColuna1 = coluna1.cloneNode(true);
+      let novaColuna2 = coluna2.cloneNode(true);
+      let novaColuna3 = btnEdit.cloneNode(true);
+      let novaColuna4 = btnDelete.cloneNode(true);
+
+      novacolunaID.textContent = item.id;
+      novaColuna1.textContent = item.coluna1;
+      novaColuna2.textContent = item.coluna2;
+      novaColuna3.textContent = "Editar";
+      novaColuna4.textContent = "Deletar";
+
+      novaColuna3.addEventListener("click", () => {
+        if (novaColuna3.textContent === "Editar") {
+          novaColuna1.innerHTML = '<input type="text" value="' + novaColuna1.textContent + '">';
+          novaColuna2.innerHTML = '<input type="text" value="' + novaColuna2.textContent + '">';
+          novaColuna3.textContent = "Salvar";
+          novaColuna4.textContent = "Cancelar";
+        } else {
+          let dadosLinha = {
+            colunaId: novacolunaID.textContent,
+            coluna1: novaColuna1.querySelector("input").value,
+            coluna2: novaColuna2.querySelector("input").value
+          };
+
+          lt_data[i] = dadosLinha;
+
+          novaColuna1.textContent = dadosLinha.coluna1;
+          novaColuna2.textContent = dadosLinha.coluna2;
+          novaColuna3.textContent = "Editar";
+          novaColuna4.textContent = "Deletar";
+        }
+      });
+
+      novaColuna4.addEventListener("click", () => {
+        if (novaColuna4.textContent === "Cancelar") {
+          if (novaColuna1.querySelector("input").value !== novaColuna1.textContent) {
+            novaColuna1.innerHTML = novaColuna1.querySelector("input").value;
+            novaColuna2.innerHTML = novaColuna2.querySelector("input").value;
+          } else {
+            novaColuna1.innerHTML = item.coluna1;
+            novaColuna2.innerHTML = item.coluna2;
+          }
+
+          novaColuna3.textContent = "Editar";
+          novaColuna4.textContent = "Deletar";
+        } else {
+          linha.remove();
+        }
+      });
+
+      linha.appendChild(novacolunaID);
+      linha.appendChild(novaColuna1);
+      linha.appendChild(novaColuna2);
+      linha.appendChild(novaColuna3);
+      linha.appendChild(novaColuna4);
+      table.appendChild(linha);
+    });
+  });
+
+let tableContainer = document.getElementById("table-container");
+tableContainer.appendChild(table);
+
+console.log(lt_data);
 
 
 
